e7e2c98d6d3c3af402284082a37e1813521a9993
danielreboucas <danielreboucasq@gmail.com>
2019-09-10 22:35:55 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+    
+template  <typename T> bool inicializar(Deque<T> &D)
+{
+
+}
+
+template  <typename T> void terminar(Deque<T> &D)
+{
+
+}
+
+template  <typename T> bool vazio(Deque<T> &D)
+{
+    return 0; 
+}
+
+template  <typename T> bool inserir_esq(Deque<T> &D, T e)
+{
+    return 0; 
+}
+
+template  <typename T> bool inserir_dir(Deque<T> &D, T e)
+{
+
+}
+
+template  <typename T> T remover_esq(Deque<T> &D)
+{
+
+}
+
+template  <typename T> T remover_dir(Deque<T> &D)
+{
+
+};
+
+
+void main()
+{
+ 
+};
