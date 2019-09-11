2fd611846535f32e3ffd71b29d6ce97e6b231809
danielreboucas <danielreboucasq@gmail.com>
2019-09-10 22:39:32 -0300

    "returns"

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 using std::cin;
 using std::cout;
@@ -7,37 +5,37 @@ using std::cout;
     
 template  <typename T> bool inicializar(Deque<T> &D)
 {
-
+    return false;
 }
 
 template  <typename T> void terminar(Deque<T> &D)
 {
-
+    return ;
 }
 
 template  <typename T> bool vazio(Deque<T> &D)
 {
-    return 0; 
+    return false; 
 }
 
 template  <typename T> bool inserir_esq(Deque<T> &D, T e)
 {
-    return 0; 
+    return false; 
 }
 
 template  <typename T> bool inserir_dir(Deque<T> &D, T e)
 {
-
+    return false;
 }
 
 template  <typename T> T remover_esq(Deque<T> &D)
 {
-
+    return ;
 }
 
 template  <typename T> T remover_dir(Deque<T> &D)
 {
-
+    return ; 
 };
 
 
